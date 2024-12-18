3ba5ea419e85315bfbdeaf1076b13c96bf8b0e90
jayonthenet <clemens@humanitec.com>
2024-12-18 11:26:38 +0100

    Add support for score-k8s installation and clean up postCreateCommand script
    Signed-off-by: jayonthenet <clemens@humanitec.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,6 +9,15 @@ else
     echo "Docker daemon is already running."
 fi
 
+# For score-k8s AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -sLO "https://github.com/score-spec/score-k8s/releases/download/0.1.18/score-k8s_0.1.18_linux_amd64.tar.gz"
+# For score-k8s ARM64
+[ $(uname -m) = aarch64 ] && curl -sLO "https://github.com/score-spec/score-k8s/releases/download/0.1.18/score-k8s_0.1.18_linux_arm64.tar.gz"
+tar xvzf score-k8s*.tar.gz
+rm score-k8s*.tar.gz README.md LICENSE
+sudo mv ./score-k8s /usr/local/bin/score-k8s
+sudo chown root: /usr/local/bin/score-k8s
+
 # For Kubectl AMD64 / x86_64
 [ $(uname -m) = x86_64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 # For Kubectl ARM64
@@ -32,7 +41,14 @@ echo "complete -F __start_kubectl k" >> $HOME/.bashrc
 docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
 
 # Install idpbuilder
+mkdir -p $HOME/temp
+cd $HOME/temp
 curl -fsSL https://raw.githubusercontent.com/cnoe-io/idpbuilder/main/hack/install.sh | bash
+cd ..
+rm -rf temp
 
 # Run idpbuilder with the specified command
 idpbuilder create --use-path-routing --package https://github.com/cnoe-io/stacks//ref-implementation
+
+# Prep env
+kind export kubeconfig --name=localdev
